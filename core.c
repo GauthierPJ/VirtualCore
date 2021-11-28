03e8c3b81affe401e89166d9658d06fdd2c7f4d1
amstros <thomas.serabian@gmail.com>
2021-11-28 19:41:16 +0100

    started core

--- core.c
+++ core.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 void fetch(){
 
 }
@@ -14,7 +14,36 @@ void execute(){
 
 
 int main(int argc, char *argv[]){
+    
+    // verify that the right number of arguments were specifide, exits otherwise
+    if(argc < 2){
+        printf("Error : An executable file must be specified!\n");
+        printf("Hint : ./core <filename>\n");
+        return 0;
+    }
+    if(argc > 2){
+        printf("Error : Only one file should be specified!\n");
+        printf("Hint : ./core <filename>\n");
+        return 0;
+    }
 
+    // open the given file
+    FILE* f;
+    f = fopen(argv[1],"rb");
+    
+    // check the file could be opened or exits
+    if (f==NULL){
+        printf("Error : Something went wrong opening the specified file\n");
+        return 0;
+    }
+    
+
+    const size_t line_size = 300;
+    char* line = malloc(line_size);
+    fread(line, line_size, 200, f);
+    printf("%s",line);
+    free(line);
+    
 }
 
 
