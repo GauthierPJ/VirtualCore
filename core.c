73f1f34eb8c8e1f5a91c17564b46828dceead7dd
GauthierPJ <petitjeang@eisti.eu>
2021-11-29 17:43:45 +0100

    Function create ins tab

--- core.c
+++ core.c
@@ -16,7 +16,7 @@ void execute(){
 
 }
 
-void print_ins(char** instructions, const unsigned int nb_ins ){
+void print_ins(char** instructions, unsigned int nb_ins ){
     for(unsigned int i = 0 ; i < nb_ins ; i++){
         printf("%03d : ", i);
         for(unsigned char y = 0 ; y < INS_SIZE ; y++){
@@ -33,17 +33,10 @@ void free_2d(char** array, unsigned int len){
     free(array);
 }
 
-int main(int argc, char *argv[]){ 
-    
-    // verify that the right number of arguments were specifide, exits otherwise
-    if(argc != 2){
-        printf("Error : An executable file must be specified!\n");
-        printf("Hint : ./core <filename>\n");
-        return 0;
-    }
-    // open the given file
+char** init_ins(char* argv, unsigned int* ptr_nb_ins){
+     // open the given file
     FILE* f;
-    f = fopen(argv[1],"rb");
+    f = fopen(argv,"rb");
     
     // check the file could be opened or exits
     if (f==NULL){
@@ -63,45 +56,48 @@ int main(int argc, char *argv[]){
 
     //n instructions -> each instruction is n/4 bytes
     const unsigned int nb_ins = nb_bytes_to_read/INS_SIZE;
-    char ** instrutions = (char**) malloc(nb_ins*sizeof(char*));
+    
+    char ** instructions = (char**) malloc(nb_ins*sizeof(char*));
     
     for(unsigned char i = 0; i < nb_ins ; i++){
-        instrutions[i] = (char*) malloc(INS_SIZE * sizeof(char));
+        instructions[i] = (char*) malloc(INS_SIZE * sizeof(char));
     }
 
     for (unsigned int l = 0; l < nb_ins; l++){
       // for each insctruction
       for (unsigned char b = 0; b < INS_SIZE; b++){
         // save the 4 bytes in a cell
-        instrutions[l][b] = program[(l*4)+b];
+        instructions[l][b] = program[(l*4)+b];
       }
     }
-
-    // // print table ** TEMPORARY **
-    // for (unsigned int l = 0; l < nb_ins; l++){
-    //   // for each insctruction
-    //   for (unsigned char b = 0; b < INS_SIZE; b++){
-    //     // save the 4 bytes in a cell
-    //     printf("%hhx ",instrutions[l][b]);
-    //   }
-    //   printf("\n");
-    // }
-    
-    print_ins(instrutions, nb_ins);
-
-    free_2d(instrutions, nb_ins);
-
-    //char** instructions = read_bytes_from_file();
-
-    
-    // printf("Number of bytes to read : %ld \n",nb_bytes_to_read);
-    // // read each line
-    //     for(int i=0; i < nb_bytes_to_read;i++){
-    //         printf("%hhx : ",program[i]);
-        
-    //     }
-    
+    // Modify the value of the pointer
+    *ptr_nb_ins = nb_ins;
     free(program);
+    return instructions;
+}
+
+int main(int argc, char *argv[]){ 
+    
+    // verify that the right number of arguments were specifide, exits otherwise
+    if(argc != 2){
+        printf("Error : An executable file must be specified!\n");
+        printf("Hint : ./core <filename>\n");
+        return 0;
+    }
+
+    
+
+   
+    unsigned int nb_ins = 0;
+    unsigned int* ptr_nb_ins = &nb_ins;
+
+    
+    char** instructions = init_ins(argv[1], ptr_nb_ins);
+
+    print_ins(instructions, nb_ins);
+
+    printf("Value = %d \n", nb_ins);  
+    
     return 0;
     
 }
